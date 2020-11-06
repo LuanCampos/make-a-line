1f985f6174332e030fcaf090e39754390b0282b7
LuanCampos <43451938+LuanCampos@users.noreply.github.com>
2020-11-05 23:27:13 -0300

    Add Shapes spin

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject[] shapes;
+	
+	private GameObject shape;
+	private Vector3 spawnPos = new Vector3(10, 0, 0);
+	private int index;
+	
+    void Start()
+    {
+        SpawnShape();
+    }
+	
+	private void SpawnShape()
+	{
+		index = Random.Range(0, shapes.Length);
+        shape = shapes[index];
+		GameObject newShape = Instantiate (shape, spawnPos, Quaternion.Euler(new Vector3(0, 0, 0)));
+	}
+	
+}
---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+	private Rigidbody2D rb;
+	//private Vector2 moveSpeed;
+	//private float rotationSpeed;
+	
+	void Start()
+	{
+		rb = this.GetComponent<Rigidbody2D>();
+		//moveSpeed = ;
+		//rotationSpeed = ;
+	}
+	
+    void FixedUpdate()
+    {
+        if (transform.position.y < -9f || transform.position.y > 9f)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y * -.95f, transform.position.z);
+		}
+		
+		rb.rotation += .5f;
+		rb.position += new Vector2(.01f, .01f);
+    }
+	
+}
