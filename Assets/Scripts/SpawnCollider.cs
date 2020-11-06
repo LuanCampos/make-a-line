230724f564cf9ac30cb87c7d2e42a8e4b3f8bf60
LuanCampos <43451938+LuanCampos@users.noreply.github.com>
2020-11-06 15:08:19 -0300

    Update Spawn colliders

--- Assets/Scripts/SpawnCollider.cs
+++ Assets/Scripts/SpawnCollider.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class SpawnCollider : MonoBehaviour
 {
+	[SerializeField]
+	private GameObject spawnColliderPreset;
+	
     private SpawnManager spawnManager;
+	private float scale;
 	
     void Start()
     {
@@ -15,18 +19,53 @@ public class SpawnCollider : MonoBehaviour
     {
 		if (col.gameObject.tag == "Ball")
 		{
-			if (transform.position.x >= 0f)
+			if (transform.position.x > 0f)
 			{
-				spawnManager.SpawnShape(new Vector3(transform.position.x + 20f, 0, 0));
+				scale = 1f;				
 			}
 			
 			else
 			{
-				spawnManager.SpawnShape(new Vector3(transform.position.x - 20f, 0, 0));
+				scale = -1f;
 			}
 			
+			SpawnShape(20f * scale);
+			SpawnNextCollider();
 			Destroy(gameObject);
-			
+		}
+		
+	}
+	
+	private void SpawnShape(float offset)
+	{
+		spawnManager.SpawnShape(new Vector3(transform.position.x + offset, 0, 0));
+	}
+	
+	private void InstantiateCollider(float offset)
+	{
+		Instantiate(spawnColliderPreset, new Vector3(transform.position.x + offset, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+	}
+	
+	private void SpawnNextCollider()
+	{
+		if (Mathf.Abs(transform.position.x) > 85)
+		{
+			InstantiateCollider(7f * scale);
+		}
+		
+		else if (Mathf.Abs(transform.position.x) > 61)
+		{
+			InstantiateCollider(8f * scale);
+		}
+		
+		else if (Mathf.Abs(transform.position.x) > 34)
+		{
+			InstantiateCollider(9f * scale);
+		}
+		
+		else
+		{
+			InstantiateCollider(10f * scale);
 		}
 		
 	}
