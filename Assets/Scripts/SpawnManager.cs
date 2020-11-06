dc861fdf3b6ea650528df114c3c2567b9500549f
LuanCampos <43451938+LuanCampos@users.noreply.github.com>
2020-11-06 16:23:46 -0300

    Update initial spawn

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
 	public GameObject[] shapes;
+	public GameObject getPoint;
+	public GameObject getTime;
+	public GameObject loseTime;
 	
     void Start()
     {
-        SpawnShape(new Vector3(10, 0, 0));
+		InitialSpawn();
     }
 	
 	public void SpawnShape(Vector3 spawnPos)
 	{
 		Instantiate (shapes[Random.Range(0, shapes.Length)], spawnPos, Quaternion.Euler(new Vector3(0, 0, 0)));
+	}
+	
+	public void SpawnGetPoint(Vector3 spawnPos)
+	{
+		Instantiate (getPoint, spawnPos, Quaternion.Euler(new Vector3(0, 0, 0)));
+	}
+	
+	public void SpawnGetTime(Vector3 spawnPos)
+	{
+		Instantiate (getTime, spawnPos, Quaternion.Euler(new Vector3(0, 0, 0)));
+	}
+	
+	public void SpawnLoseTime(Vector3 spawnPos)
+	{
+		Instantiate (loseTime, spawnPos, Quaternion.Euler(new Vector3(0, 0, 0)));
+	}
+	
+	private void InitialSpawn()
+	{
+		SpawnShape(new Vector3(10, 0, 0));	
+		SpawnGetPoint(new Vector3(Random.Range(-7f, -2f), Random.Range(-2.5f, 2.5f), 0));
+		SpawnGetPoint(new Vector3(Random.Range(3f, 12f), Random.Range(-2.5f, 2.5f), 0));
 	}
 	
 }
