928d59866a6cacef2c73bf478a849a200be048aa
LuanCampos <43451938+LuanCampos@users.noreply.github.com>
2020-11-05 23:36:21 -0300

    Update Spin.cs

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class Spin : MonoBehaviour
 {
 	private Rigidbody2D rb;
-	//private Vector2 moveSpeed;
-	//private float rotationSpeed;
+	private Vector2 moveSpeed;
+	private float rotationSpeed;
 	
 	void Start()
 	{
 		rb = this.GetComponent<Rigidbody2D>();
-		//moveSpeed = ;
-		//rotationSpeed = ;
+		moveSpeed = new Vector2(Random.Range(-.015f, .015f), Random.Range(-.015f, .015f));
+		rotationSpeed = Random.Range(-.65f, .65f);
 	}
 	
     void FixedUpdate()
@@ -22,8 +22,8 @@ public class Spin : MonoBehaviour
 			transform.position = new Vector3(transform.position.x, transform.position.y * -.95f, transform.position.z);
 		}
 		
-		rb.rotation += .5f;
-		rb.position += new Vector2(.01f, .01f);
+		rb.rotation += rotationSpeed;
+		rb.position += moveSpeed;
     }
 	
 }
