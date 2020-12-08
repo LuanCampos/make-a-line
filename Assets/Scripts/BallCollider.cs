cf96684ed565915e851d679fa9b11868391f5b34
LuanCampos <43451938+LuanCampos@users.noreply.github.com>
2020-12-08 14:59:57 -0300

    Add ball colliders

---
+++ Assets/Scripts/BallCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag != "Spawner")
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
